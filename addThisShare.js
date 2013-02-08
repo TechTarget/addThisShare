12acee58dfacb54f2000a91af691b53579c9bc46
Morgan Wigmanich <okize123@gmail.com>
2013-02-08 13:00:44 -0500

    bumped version number

--- addThisShare.js
+++ addThisShare.js
@@ -1,5 +1,5 @@
 /*!
-* AddThis Share v1.0.2 (http://okize.github.com/)
+* AddThis Share v1.0.3 (http://okize.github.com/)
 * Copyright (c) 2013 | Licensed under the MIT license - http://www.opensource.org/licenses/mit-license.php
 */
 
